985cc71af646c9bb767bb4128bc48250606abc53
wingyuen2022 <wingyuenuk330@gmail.com>
2022-07-07 15:53:17 +0100

    Fixed random font color

--- src/components/MusicPicker/index.js
+++ src/components/MusicPicker/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import Invert from 'invert-color';
 import MusicItem from '../MusicItem';
 import { MusicContext } from '../MusicContext/MusicContext';
 
@@ -67,8 +68,12 @@ function MusicPicker() {
     if (r > 256) {r = r % 256;}
     if (g > 256) {g = g % 256;}
     if (b > 256) {b = b % 256;}
-    document.body.style.color = `rgb(${256-r}, ${256-g}, ${256-b})`;
     document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+    let hex = Invert([r,g,b]);
+    r = parseInt( hex.substring( 1, 3 ), 16 );
+    g = parseInt( hex.substring( 3, 5 ), 16 );
+    b = parseInt( hex.substring( 5, 7 ), 16 );
+    document.body.style.color = `rgb(${r}, ${g}, ${b})`;
     renderRow();
   }, [curSong]);
 
